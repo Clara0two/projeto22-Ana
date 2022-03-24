eee3ce2695d7bf0ce470449c84a52c31597e371b
Clara0two <acleite2007@gmail.com>
2022-03-24 20:04:40 -0300

    first commit

---
+++ sketch.js
@@ -0,0 +1,52 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var canvas;
+var player, playerBase, playerArcher;
+var baseimage;
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.png");
+  baseimage = loadImage("./assets/base.png");
+  playerimage = loadImage("./assets/player.png");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+
+  engine = Engine.create();
+  world = engine.world;
+  angleMode(DEGREES);
+
+  //criar corpo da base do jogador
+playerBase=Bodies.rectangle(150,350,150,100,{isStatic:true});
+World.add(world,playerBase)
+player=Bodies.rectangle(180,200,50,180,{isStatic:true});
+World.add(world,player)
+  //criar corpo do jogador
+
+
+
+}
+
+function draw() {
+  background(backgroundImg);
+
+  //exibir a imagem do jogador usando a função image
+  
+  image(baseimage,playerBase.position.x,playerBase.position.y,150,100)
+  image(playerimage,player.position.x,player.position.y,50,180)
+  //exibir a imagem da base do jogador usando a função image()
+
+
+  Engine.update(engine);
+
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("ARQUEIRO ÉPICO", width / 2, 100);
+}
